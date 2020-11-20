58e5bd183c048676fc6329b019fb12365b758e0a
indexedakki <74480055+indexedakki@users.noreply.github.com>
2020-11-20 09:53:28 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
-
-from app import flask_app
+from database import db
+from app import app
 
 # flask_app = Flask(__name__, template_folder='html')
 
-if __name__ == '__main__':
-    flask_app.run(debug = True, port=8501)
+
+app = Flask(__name__, template_folder='html')
+#Bootstrap(flask_app)
+app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/data.db'
+app.config['SQLALCHEMY_BINDS'] = {
+    'contact': 'sqlite:///database/contact.db',
+    'issuebook': 'sqlite:///database/issuebook.db'
+                                       }
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
